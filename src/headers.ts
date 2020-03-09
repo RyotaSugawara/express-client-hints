38e2649528911c9cce883f3dc0513ea737f047a0
sugawara-ryota <sugawara_ryota@cyberagent.co.jp>
2020-03-09 22:18:01 +0900

    feat: use contants

---
+++ src/headers.ts
@@ -0,0 +1,25 @@
+/**
+ * @see {@link https://tools.ietf.org/html/draft-ietf-httpbis-client-hints-10#section-3}
+ * @description
+ * Accept-CH = sh-list
+ * example
+ * ```
+ * Accept-CH: Sec-CH-Example, Sec-CH-Example-2
+ * ```
+ */
+export const CLIENT_HINTS_RESPONSE_HEADER_MAP = {
+  ACCEPT_CH: 'Accept-CH',
+};
+
+/**
+ * User Agent Hints
+ * @see {@link https://wicg.github.io/ua-client-hints/#http-ua-hints}
+ */
+export const CLIENT_HINTS_REQUEST_HEADER_MAP = {
+  SEC_CH_UA: 'Sec-CH-UA',
+  SEC_CH_UA_ARCH: 'Sec-CH-UA-Arch',
+  SEC_CH_UA_MOBILE: 'Sec-CH-UA-Mobile',
+  SEC_CH_UA_MODEL: 'Sec-CH-UA-Model',
+  SEC_CH_UA_PLATFORM: 'Sec-CH-UA-Platform',
+  SEC_CH_UA_PLATFORM_VERSION: 'Sec-CH-UA-Platform-Version',
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Handler, Request, Response, NextFunction } from 'express';
 import { clientHintsUserAgentParser, UABrandVersion } from './parser';
+import {
+  CLIENT_HINTS_RESPONSE_HEADER_MAP,
+  CLIENT_HINTS_REQUEST_HEADER_MAP,
+} from './headers';
 
 /**
  * Override Express.Request to create new Request fields
@@ -35,32 +39,6 @@ export interface ClientHintsDetail {
  */
 export type StructuredHeadersBoolean = '?0' | '?1';
 
-/**
- * @see {@link https://tools.ietf.org/html/draft-ietf-httpbis-client-hints-10#section-3}
- * @description
- * Accept-CH = sh-list
- * example
- * ```
- * Accept-CH: Sec-CH-Example, Sec-CH-Example-2
- * ```
- */
-const CLIENT_HINTS_RESPONSE_HEADER_MAP = {
-  ACCEPT_CH: 'Accept-CH',
-};
-
-/**
- * User Agent Hints
- * @see {@link https://wicg.github.io/ua-client-hints/#http-ua-hints}
- */
-const CLIENT_HINTS_REQUREST_HEADER_MAP = {
-  SEC_CH_UA: 'Sec-CH-UA',
-  SEC_CH_UA_ARCH: 'Sec-CH-UA-Arch',
-  SEC_CH_UA_MOBILE: 'Sec-CH-UA-Mobile',
-  SEC_CH_UA_MODEL: 'Sec-CH-UA-Model',
-  SEC_CH_UA_PLATFORM: 'Sec-CH-UA-Platform',
-  SEC_CH_UA_PLATFORM_VERSION: 'Sec-CH-UA-Platform-Version',
-};
-
 const clientHintsHandler = (accepts: string[] = []): Handler => (
   req: Request,
   res: Response,
@@ -90,10 +68,10 @@ const clientHintsHandler = (accepts: string[] = []): Handler => (
    * e.g. Sec-CH-UA: "Chrome"; v="82", "NotBrowser"; v="0"
    */
   const shListUserAgent =
-    req.get(CLIENT_HINTS_REQUREST_HEADER_MAP.SEC_CH_UA) || '';
+    req.get(CLIENT_HINTS_REQUEST_HEADER_MAP.SEC_CH_UA) || '';
   const brandVersionList = clientHintsUserAgentParser(shListUserAgent);
   const mobileString = req.get(
-    CLIENT_HINTS_REQUREST_HEADER_MAP.SEC_CH_UA_MOBILE,
+    CLIENT_HINTS_REQUEST_HEADER_MAP.SEC_CH_UA_MOBILE,
   );
 
   if (!mobileString) {
@@ -112,13 +90,13 @@ const clientHintsHandler = (accepts: string[] = []): Handler => (
   req.clientHints._brandVersions = brandVersionList;
 
   req.clientHints.mobile =
-    req.get(CLIENT_HINTS_REQUREST_HEADER_MAP.SEC_CH_UA_MOBILE) === '?1';
+    req.get(CLIENT_HINTS_REQUEST_HEADER_MAP.SEC_CH_UA_MOBILE) === '?1';
 
-  const architecture = req.get(CLIENT_HINTS_REQUREST_HEADER_MAP.SEC_CH_UA_ARCH);
-  const model = req.get(CLIENT_HINTS_REQUREST_HEADER_MAP.SEC_CH_UA_MODEL);
-  const platform = req.get(CLIENT_HINTS_REQUREST_HEADER_MAP.SEC_CH_UA_PLATFORM);
+  const architecture = req.get(CLIENT_HINTS_REQUEST_HEADER_MAP.SEC_CH_UA_ARCH);
+  const model = req.get(CLIENT_HINTS_REQUEST_HEADER_MAP.SEC_CH_UA_MODEL);
+  const platform = req.get(CLIENT_HINTS_REQUEST_HEADER_MAP.SEC_CH_UA_PLATFORM);
   const platformVersion = req.get(
-    CLIENT_HINTS_REQUREST_HEADER_MAP.SEC_CH_UA_PLATFORM_VERSION,
+    CLIENT_HINTS_REQUEST_HEADER_MAP.SEC_CH_UA_PLATFORM_VERSION,
   );
   if (architecture !== undefined) {
     req.clientHints.architecture = architecture;
@@ -137,3 +115,7 @@ const clientHintsHandler = (accepts: string[] = []): Handler => (
 };
 
 export default clientHintsHandler;
+export {
+  CLIENT_HINTS_REQUEST_HEADER_MAP,
+  CLIENT_HINTS_RESPONSE_HEADER_MAP,
+};
